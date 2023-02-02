efc2afc08e0ad78506227c1b6749c685b1e769d4
Julie Viktoria <julieviktoria1@hotmail.com>
2023-02-02 03:02:24 +0100

    🚀​ set up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("hello world!");
+let products = [ 
+    {
+        name: "Ymse Gold Chain",
+        tag: "goldchain",
+        price: 499,
+        inCart: 0
+    },
+    {
+        name: "Ellie Gold Hoops",
+        tag: "1goldhoops",
+        price: 299,
+        inCart: 0
+    },
+    {
+        name: "Ada Gold Set",
+        tag: "goldset",
+        price: 749,
+        inCart: 0
+    },
+    {
+        name: "Eira Gold Hoops",
+        tag: "2goldhoops",
+        price: 20,
+        inCart: 0
+    }
+];
